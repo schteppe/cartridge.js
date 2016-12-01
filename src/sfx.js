aadf9eaa4a9762b50e130efbccfd3c13bab05c6c
Stefan Hedman <schteppe@gmail.com>
2016-12-01 22:04:14 +0100

    code cleanup

--- src/sfx.js
+++ src/sfx.js
@@ -114,7 +114,6 @@ function play(channel, types, frequencies, volumes, speed, offset){
 	}
 	if(endPosition === 0) return;
 
-	var len = (endPosition - offset) / speed;
 	var currentTime = context.currentTime;
 	for(i=offset; i<endPosition; i++){
 		var type = allTypes[types[i]];
@@ -122,22 +121,25 @@ function play(channel, types, frequencies, volumes, speed, offset){
 
 		var startTime = currentTime + i / speed;
 
-		// Set other gains to zero
 		for(j=0; j<allTypes.length; j++){
 			gain = channel.gains[allTypes[j]];
+
+			// Set other gains to zero
 			if(allTypes[j] !== type){
 				gain.gain.setValueAtTime(0, startTime);
 			} else {
-				if(osc.frequency){
+				if(osc.frequency){ // noise doesn't have frequency
 					osc.frequency.setValueAtTime(frequencies[i] / 255 * (maxFrequency - minFrequency) + minFrequency, startTime);
 				}
-				gain.gain.setValueAtTime(volumes[i] / 255, startTime);
+				var vol = volumes[i] / 255;
+				gain.gain.setValueAtTime(vol, startTime);
 			}
 		}
 	}
 
 	// Set the volume at the end to zero
-	var endTime = currentTime + (endPosition-offset) / speed;
+	var len = (endPosition-offset) / speed;
+	var endTime = currentTime + len;
 	for(j=0; j<allTypes.length; j++){
 		gain = channel.gains[allTypes[j]];
 		gain.gain.setValueAtTime(0, endTime);
