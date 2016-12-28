edd975e5b728c2ce79f4303f497a8f9060fa5b51
Stefan Hedman <schteppe@gmail.com>
2016-12-28 14:31:29 +0100

    work on music

--- src/music.js
+++ src/music.js
@@ -6,8 +6,13 @@ var sfx = require('./sfx');
  * Source: http://www.phy.mtu.edu/~suits/notefreqs.html
  */
 var frequencies = [16.35, 17.32, 17.32, 18.35, 19.45, 19.45, 20.60, 21.83, 23.12, 23.12, 24.50, 25.96, 25.96, 27.50, 29.14, 29.14, 30.87, 32.70, 34.65, 34.65, 36.71, 38.89, 38.89, 41.20, 43.65, 46.25, 46.25, 49.00, 51.91, 51.91, 55.00, 58.27, 58.27, 61.74, 65.41, 69.30, 69.30, 73.42, 77.78, 77.78, 82.41, 87.31, 92.50, 92.50, 98.00, 103.83, 103.83, 110.00, 116.54, 116.54, 123.47, 130.81, 138.59, 138.59, 146.83, 155.56, 155.56, 164.81, 174.61, 185.00, 185.00, 196.00, 207.65, 207.65, 220.00, 233.08, 233.08, 246.94, 261.63, 277.18, 277.18, 293.66, 311.13, 311.13, 329.63, 349.23, 369.99, 369.99, 392.00, 415.30, 415.30, 440.00, 466.16, 466.16, 493.88, 523.25, 554.37, 554.37, 587.33, 622.25, 622.25, 659.26, 698.46, 739.99, 739.99, 783.99, 830.61, 830.61, 880.00, 932.33, 932.33, 987.77, 1046.50, 1108.73, 1108.73, 1174.66, 1244.51, 1244.51, 1318.51, 1396.91, 1479.98, 1479.98, 1567.98, 1661.22, 1661.22, 1760.00, 1864.66, 1864.66, 1975.53, 2093.00, 2217.46, 2217.46, 2349.32, 2489.02, 2489.02, 2637.02, 2793.83, 2959.96, 2959.96, 3135.96, 3322.44, 3322.44, 3520.00, 3729.31, 3729.31, 3951.07, 4186.01];
-
 var noteNames = "C C# Db D D# Eb E F F# Gb G G# Ab A A# Bb B".split(' ');
+var PatternFlags = {
+	ACTIVE: 1,
+	THROW: 2,
+	CATCH: 4,
+	STOP: 8
+};
 
 // pitch: 0-16. Octave: 0-8
 function getFrequency(pitch, octave){
@@ -83,15 +88,14 @@ exports.mgget = function(pattern, channel){
 	return patterns[pattern * 5 + 1 + channel];
 };
 
-// Set stop/repeat flags for a pattern: 1=stop, 2=catch, 4=throw
+// Set flags for a pattern
 exports.mfset = function(pattern, flags){
 	patterns[pattern * 5 + 0] = flags;
 };
 
-// Play or stop music
-exports.music = function(pattern, fade, channelmask){
-	fade = fade !== undefined ? fade : 0;
-	channelmask = channelmask !== undefined ? channelmask : 0;
+// Get flags
+exports.mfget = function(pattern){
+	return patterns[pattern * 5 + 0];
 };
 
 // Create audio stuff
@@ -181,5 +185,42 @@ function scheduleGroup(groupIndex, channelIndex, time){
 		gain.gain.setValueAtTime(0, endTime);
 
 		i = endPosition - 1;
+	}
+}
+
+// Play or stop music
+exports.music = function(patternIndex, fade, channelmask){
+	fade = fade !== undefined ? fade : 0;
+	channelmask = channelmask !== undefined ? channelmask : 0;
+	if(patternIndex === -1){
+		stop(fade);
+		return;
+	}
+
+	// schedule all groups
+	var startTime = context.currentTime;
+	for(var patternIndex=0; patternIndex < patterns.length; patternIndex++){
+		var flags = mfget(patternIndex);
+		if(!(flags & PatternFlags.ACTIVE)) continue;
+		for(var channelIndex=0; channelIndex < channels.length; channelIndex++){
+			var groupIndex = mgget(patternIndex, channelIndex);
+			var speed = gsget(groupIndex);
+			scheduleGroup(groupIndex, channelIndex, startTime);
+		}
+	}
+};
+
+function stop(fade){
+	var currentTime = context.currentTime;
+	for(var channelIndex=0; channelIndex<channels.length; channelIndex++){
+		var channel = channels[channelIndex];
+		for(var i=0; i<allTypes.length; i++){
+			var instrument = channel.instruments[allTypes[i]];
+			if(instrument.frequency){
+				instrument.frequency.cancelAllScheduledValues();
+			}
+			instrument.gain.cancelAllScheduledValues();
+			instrument.gain.setValueAtTime(0, currentTime);
+		}
 	}
 }
