576345d7a917f16637b2c3a347e3e304be966397
Stefan Hedman <schteppe@gmail.com>
2017-07-23 18:42:40 +0200

    move Buttons to own file, only show JS error if code is running

---
+++ src/editor/Buttons.js
@@ -0,0 +1,61 @@
+var Rectangle = require('../Rectangle');
+var utils = require('../utils');
+
+module.exports = Buttons;
+
+function Buttons(options){
+	options = options || {};
+	Rectangle.call(this);
+
+	this.options = options.options || [];
+
+	if(options.x) this.x = options.x;
+	if(options.y) this.y = options.y;
+	if(options.sx) this.sx = options.sx;
+	if(options.sy) this.sy = options.sy;
+	if(options.onclick) this.onclick = options.onclick;
+
+	this.padding = options.padding !== undefined ? options.padding : 0;
+	this.current = options.current !== undefined ? options.current : 0;
+	this.num = options.num !== undefined ? options.num : undefined;
+	this.bgColor = options.bgColor !== undefined ? options.bgColor : undefined;
+	this.textColor = options.textColor !== undefined ? options.textColor : undefined;
+}
+Buttons.prototype = Object.create(Rectangle.prototype);
+Object.assign(Buttons.prototype, {
+	draw: function(){
+		var padding = this.padding !== undefined ? this.padding : 4;
+		var num = this.num || this.options.length;
+		var bgColor = this.bgColor !== undefined ? this.bgColor : 0;
+		var textColor = this.textColor !== undefined ? this.textColor : 6;
+		for(var i=0; i<num; i++){
+			var x0 = this.x() + i * (6 + padding*2);
+			rectfill(
+				x0, this.y(),
+				this.x() + 5+padding*2 + i * (6+padding*2)-1, this.y() + 6,
+				this.current === i ? bgColor : textColor
+			);
+			var text = this.options !== undefined ? (this.options[i]+'').toUpperCase() : ((i+1) + '');
+			var x1 = this.x()+1+padding + i * (6 + padding*2);
+			print(
+				text,
+				this.options !== undefined ? (x0+1) : x1, this.y()+1,
+				this.current === i ? textColor : bgColor
+			);
+		}
+	},
+	click: function(x,y){
+		var num = this.num !== undefined ? this.num : this.options.length;
+		if(utils.inrect(x,y,this.x(),this.y(), num * (this.padding * 2 + 6),7)){
+			var button = flr((x-this.x()) / (this.padding * 2 + 6));
+			if(this.current === button){
+				return false;
+			} else {
+				this.current = button;
+				return true;
+			}
+		}
+		return false;
+	},
+	onclick: function(){}
+});
--- src/editor/index.js
+++ src/editor/index.js
@@ -2,6 +2,7 @@ var utils = require('../utils');
 var Editor = require('./Editor');
 var Palette = require('./Palette');
 var Flags = require('./Flags');
+var Buttons = require('./Buttons');
 var Rectangle = require('../Rectangle');
 
 var editor = new Editor({
@@ -442,63 +443,6 @@ var flags = new Flags({
 			fset(sprites.current, newFlags);
 		}
 	},
-});
-
-function Buttons(options){
-	options = options || {};
-	Rectangle.call(this);
-
-	this.options = options.options || [];
-
-	if(options.x) this.x = options.x;
-	if(options.y) this.y = options.y;
-	if(options.sx) this.sx = options.sx;
-	if(options.sy) this.sy = options.sy;
-	if(options.onclick) this.onclick = options.onclick;
-
-	this.padding = options.padding !== undefined ? options.padding : 0;
-	this.current = options.current !== undefined ? options.current : 0;
-	this.num = options.num !== undefined ? options.num : undefined;
-	this.bgColor = options.bgColor !== undefined ? options.bgColor : undefined;
-	this.textColor = options.textColor !== undefined ? options.textColor : undefined;
-}
-Buttons.prototype = Object.create(Rectangle.prototype);
-Object.assign(Buttons.prototype, {
-	draw: function(){
-		var padding = this.padding !== undefined ? this.padding : 4;
-		var num = this.num || this.options.length;
-		var bgColor = this.bgColor !== undefined ? this.bgColor : 0;
-		var textColor = this.textColor !== undefined ? this.textColor : 6;
-		for(var i=0; i<num; i++){
-			var x0 = this.x() + i * (6 + padding*2);
-			rectfill(
-				x0, this.y(),
-				this.x() + 5+padding*2 + i * (6+padding*2)-1, this.y() + 6,
-				this.current === i ? bgColor : textColor
-			);
-			var text = this.options !== undefined ? (this.options[i]+'').toUpperCase() : ((i+1) + '');
-			var x1 = this.x()+1+padding + i * (6 + padding*2);
-			print(
-				text,
-				this.options !== undefined ? (x0+1) : x1, this.y()+1,
-				this.current === i ? textColor : bgColor
-			);
-		}
-	},
-	click: function(x,y){
-		var num = this.num !== undefined ? this.num : this.options.length;
-		if(utils.inrect(x,y,this.x(),this.y(), num * (this.padding * 2 + 6),7)){
-			var button = flr((x-this.x()) / (this.padding * 2 + 6));
-			if(this.current === button){
-				return false;
-			} else {
-				this.current = button;
-				return true;
-			}
-		}
-		return false;
-	},
-	onclick: function(){}
 });
 
 var patternEndButtons = {
@@ -1507,16 +1451,18 @@ function code_draw(code){
 }
 
 window._error = function(info){
-	console.error(info);
-	code_stop(code);
+	if(editor.mode === Editor.Modes.RUN){
+		console.error(info);
+		code_stop(code);
 
-	// Handle error
-	editor.mode = Editor.Modes.CODE;
-	code.ccol = info.column - 1;
-	code.crow = info.line - 1;
-	code.errorMessage = 'L' + info.column + ' C' + info.line + ' ' + info.message;
-	code.errorTime = time();
-	editor.dirty = true;
+		// Handle error
+		editor.mode = Editor.Modes.CODE;
+		code.ccol = info.column - 1;
+		code.crow = info.line - 1;
+		code.errorMessage = 'L' + info.column + ' C' + info.line + ' ' + info.message;
+		code.errorTime = time();
+		editor.dirty = true;
+	}
 };
 
 function code_run(code){
